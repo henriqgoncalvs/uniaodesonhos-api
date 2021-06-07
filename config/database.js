b22afc45a5c3f5af77f08f41a142b0fddcc5c26a
Henrique Gonçalves <henriquesg09@hotmail.com>
2021-06-07 12:32:23 -0300

    Main: update database config

--- config/database.js
+++ config/database.js
@@ -5,12 +5,12 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'uniaodesonhos-strapi'),
-        username: env('DATABASE_USERNAME', 'uniaodesonhos-strapi'),
-        password: env('DATABASE_PASSWORD', 'uniaodesonhos@marcella'),
-        ssl: env.bool('DATABASE_SSL', false),
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: env.bool('DATABASE_SSL'),
       },
       options: {}
     },
