54ee9e31b7bdf4e2e91f01573e0fd162cf246dc4
Henrique Gonçalves <henriquesg09@hotmail.com>
2021-06-07 12:04:20 -0300

    Main: initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '91efcc3317ff00d9d90d40784b2223fc'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '44ddb074-3ceb-4868-a948-3d9f4628531a'
+};
